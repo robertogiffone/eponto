723fa058a16ff525a277159aacee7b01a4c7f18a
Francisco Roberto Giffone Nobre <roberto.giffone@gmail.com>
2016-12-21 22:17:40 -0300

    Mudança na conexão

--- class/Conexao.class.php
+++ class/Conexao.class.php
@@ -1,16 +1,12 @@
 <?php
 class Conexao {
-    private $con,$servidor='localhost',$usuario='robertog_root',$senha='2006fr',$bd='robertog_eponto';
-	//com localhost deu erro
-	//private $con,$servidor='127.0.0.1',$usuario='root',$senha='',$bd='robertog_eponto';
-    //protected $tabela;
-
+    private $con,$servidor='localhost',$usuario='root',$senha='',$bd='robertog_eponto';
+    
     public function conexao(){
         try{
         	$conexao = new PDO("mysql:host=$this->servidor;dbname=$this->bd", $this->usuario, $this->senha );
             return $conexao;
         }catch (PDOException $e){
-            //echo 'Erro ao conectar = '.$e->getMessage();
             echo 'Erro na conexao ao banco de dados';
         }
         
